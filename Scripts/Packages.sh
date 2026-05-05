b0cb4c0a6dfbc86d8ad4b68167a796f1c574ba50
xz0609 <xz0609@126.com>
2026-05-06 01:03:16 +0800

    Add files via upload

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -80,7 +80,6 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 UPDATE_PACKAGE "adguardhome" "xiaoxiao29/luci-app-adguardhome" "master" "" "luci-app-adguardhome AdGuardHome"
 UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "" "luci-app-lucky lucky"
-UPDATE_PACKAGE "dockerman" "lisaac/luci-app-dockerman" "master" "" "luci-app-dockerman"
 
 #更新软件包版本
 UPDATE_VERSION() {
